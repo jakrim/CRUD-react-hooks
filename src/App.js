e2e3fc63ea73130045443b5b541aaa63d1c07a6f
Jesse Krim <jesse.krim@gmail.com>
2019-09-12 09:31:40 -0400

    Add example react hook components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Example from './Example';
+import Example1 from './Example1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className='container'>
+    <h1>CRUD App with Hooks</h1>
+    <div className='flex-row'>
+      <div className='flex-large'>
+        <h2>Add user</h2>
+      </div>
+      <div className='flex-large'>
+        <h2>View users</h2>
+      </div>
     </div>
-  );
-}
+    <Example />
+    <Example1 />
+  </div>
+);
 
 export default App;
