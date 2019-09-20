5953564700d3c2890aa78982dd18790e95235834
Jesse Krim <jesse@Jesses-MacBook-Pro.local>
2019-09-20 12:52:29 -0400

    Create UserTable

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Example from './Example';
-import Example1 from './Example1';
-import NameForm from './NameForm';
-import UseScreenWidth from './UseScreenWidth';
-import LifeCycle from './LifeCycle';
+import React, { useState } from 'react';
+import UserTable from './tables/UserTable';
 
-const App = () => (
-  <div className='container'>
-    <h1>CRUD App with Hooks</h1>
-    <div className='flex-row'>
-      <div className='flex-large'>
-        <h2>Add user</h2>
-      </div>
-      <div className='flex-large'>
-        <h2>View users</h2>
+const App = () => {
+  const [users, setUsers] = useState([
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' }
+  ]);
+
+  return (
+    <div className='container'>
+      <h1>CRUD App with Hooks</h1>
+      <div className='flex-row'>
+        <div className='flex-large'>
+          <h2>Add user</h2>
+        </div>
+        <div className='flex-large'>
+          <h2>View users</h2>
+          <UserTable users={users} />
+        </div>
       </div>
     </div>
-    <Example />
-    <Example1 />
-    <NameForm />
-    <UseScreenWidth />
-    <LifeCycle />
-  </div>
-);
+  );
+};
 
 export default App;
---
+++ src/tables/UserTable.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const UserTable = ({ users }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {users.length > 0 ? (
+        users.map(user => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.userName}</td>
+
+            <td>
+              <button className='button muted-button'>Edit</button>
+              <button className='button muted-button'>Delete</button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No users</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+);
+
+export default UserTable;
